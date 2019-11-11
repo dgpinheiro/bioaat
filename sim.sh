bd604dd0cd6bd9dbe574c0170b59f4b84bdc2332
dgpinheiro <dgpinheiro@gmail.com>
2019-11-11 13:59:16 -0200

    Scripts for simulation

---
+++ sim.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+#              INGLÊS/ENGLISH
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  http://www.gnu.org/copyleft/gpl.html
+#
+#
+#             PORTUGUÊS/PORTUGUESE
+#  Este programa é distribuído na expectativa de ser útil aos seus
+#  usuários, porém NÃO TEM NENHUMA GARANTIA, EXPLÍCITAS OU IMPLÍCITAS,
+#  COMERCIAIS OU DE ATENDIMENTO A UMA DETERMINADA FINALIDADE.  Consulte
+#  a Licença Pública Geral GNU para maiores detalhes.
+#  http://www.gnu.org/copyleft/gpl.html
+#
+#  Copyright (C) 2012  Universidade de São Paulo
+#
+#  Universidade de São Paulo
+#  Laboratório de Biologia do Desenvolvimento de Abelhas
+#  Núcleo de Bioinformática (LBDA-BioInfo)
+#
+#  Daniel Guariz Pinheiro
+#  dgpinheiro@gmail.com
+#  http://zulu.fmrp.usp.br/bioinfo 
+#
+
+rm -f transcriptoma.fa
+
+IFS=$'\n'
+for acc in $(cat ./ACCS.txt); do
+	echo "Pegando FASTA para ${acc} ..."
+
+	esearch -db nucleotide -query ${acc} | efetch \
+   	-format fasta >> transcriptoma.fa
+done
+
+
+for biogroup in A B; do
+	for rep in 1 2; do
+		echo "Gerando reads para amostra ${biogroup} réplica ${rep} ..."
+
+		generate_fragments.py -r transcriptoma.fa \
+		   -a ./abundance_${biogroup}.txt \
+		   -o ./tmp.frags_${biogroup}_${rep} \
+		   -t 25000 \
+		   -i 300 \
+		   -s 30
+
+		cat ./tmp.frags_${biogroup}_${rep}.1.fasta | renameSeqs.pl \
+		   -if FASTA \
+		   -of FASTA \
+		   -p SAMPLE${biogroup}${rep} \
+		   -w 1000 | \
+		   sed 's/^>\(\S\+\).*/>\1/' \
+		   > ./frags_${biogroup}${rep}.fa
+
+		cat ./frags_${biogroup}${rep}.fa | simNGS -a \
+		AGATCGGAAGAGCACACGTCTGAACTCCAGTCACATCACGATCTCGTATGCCGTCTTCTGCTTG:AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT \
+		-p paired \
+		/usr/local/bioinfo/simNGS/data/s_4_0099.runfile \
+		-n 151 > ./SAMPLE${biogroup}${rep}.fastq 2> SAMPLE${biogroup}${rep}.err.txt
+
+		mkdir -p ./raw
+
+		deinterleave_pairs SAMPLE${biogroup}${rep}.fastq \
+		   -o ./raw/SAMPLE${biogroup}${rep}_R1.fastq \
+		      ./raw/SAMPLE${biogroup}${rep}_R2.fastq
+
+		rm -f ./tmp.frags_${biogroup}_${rep}.1.fasta ./frags_${biogroup}${rep}.fa ./SAMPLE${biogroup}${rep}.fastq ./SAMPLE${biogroup}${rep}.err.txt
+
+		echo "Número de reads ${biogroup}${rep} R1:" $(echo "$(cat raw/SAMPLE${biogroup}${rep}_R1.fastq | wc -l)/4" | bc)
+		echo "Número de reads ${biogroup}${rep} R2:" $(echo "$(cat raw/SAMPLE${biogroup}${rep}_R2.fastq | wc -l)/4" | bc)
+
+	done
+done
